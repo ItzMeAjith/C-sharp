f55e6f4e8a28f1653da5b4e7435cb4eeb1d50f5b
ItzMeAjith <ajithitsme358@gmail.com>
2023-04-04 18:05:57 +0530

    After Stack hasttable dictionary and queue

---
+++ Collections/HashTable/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+namespace HashTable
+{ 
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Non generic - hash table---------");
+            Hashtable ht = new Hashtable();
+            ht.Add(1, "Ajith");
+            ht.Add(3, "Hari");
+            ht.Add("SU", "Suresh");
+            ht.Add(3.6, "Ranjith");
+
+            foreach (DictionaryEntry d in ht)
+            {
+                Console.WriteLine(d.Key+" "+d.Value);
+            }
+            Console.WriteLine("generic - Dictionary ---------");
+
+            Dictionary<int,string> dic= new Dictionary<int,string>();
+            dic.Add(1, "Ajith");
+            dic.Add(2, "HAri");
+            foreach (KeyValuePair<int,string> d1 in dic)
+            {
+                Console.WriteLine(d1.Key+" "+d1.Value);
+            }
+        }
+    }
+}
---
+++ Collections/StackQueue/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StackQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Non generic Stack--------------");
+        }
+    }
+}
