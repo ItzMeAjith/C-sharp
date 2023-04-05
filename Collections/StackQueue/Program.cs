4fe7a6d0cfa8d64b35bdeed4c5995ad8ded1e6e8
ItzMeAjith <ajithitsme358@gmail.com>
2023-04-05 10:11:34 +0530

    Final

--- Collections/StackQueue/Program.cs
+++ Collections/StackQueue/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using System.Collections;
 namespace StackQueue
 {
     internal class Program
@@ -7,6 +7,40 @@ namespace StackQueue
         static void Main(string[] args)
         {
             Console.WriteLine("Non generic Stack--------------");
+            Stack st = new Stack();
+            st.Push(1);
+            st.Push(2);
+            st.Push(3);
+            st.Push(4);
+            st.Push("Ajith");
+             foreach (var item in st)
+            {
+                Console.WriteLine(item);
+            }
+
+            st.Pop();
+            foreach (var item in st)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("----------------------------------------------------------");
+            // Queue
+            Queue q = new Queue();
+            q.Enqueue(1);
+            q.Enqueue(2);
+            q.Enqueue(3);
+            q.Enqueue(4);
+            q.Enqueue("Ajith");
+            foreach (var item in q)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine("----------------------------------------------------------");
+            q.Dequeue();
+            foreach (var item in q)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
