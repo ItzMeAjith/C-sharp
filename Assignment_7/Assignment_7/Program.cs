96506e02f72051ecbcaae29251ad6a3ba5a31adb
ItzMeAjith <ajithitsme358@gmail.com>
2023-04-06 12:58:59 +0530

    Assignment 7

---
+++ Assignment_7/Assignment_7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StringConcate
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter first name : ");
+            string fn=Console.ReadLine();
+            Console.Write("Enter last name : ");
+            string ln=Console.ReadLine();
+            //string fullname=(string)fn.Concat(ln);
+            //Console.WriteLine(string.Concat(fn, ln));
+            Console.WriteLine(fn+" "+ln);
+        }
+    }
+}
---
+++ Assignment_7/StringReverse/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StringReverse
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the statement to be reversed : ");
+            string s=Console.ReadLine();
+            Console.Write("The statement after reversed is : ");
+
+            int f = 0,p=0,l=0;
+            l = s.Length - 1;
+            for (int i=s.Length-1;i>=0;i--)
+            {
+                if (s[i]==' ')
+                {
+                    f = i + 1;
+                    for (int j=f;j<=l;j++)
+                    {
+                        Console.Write(s[j]);
+                    }
+                    Console.Write(" ");
+                    l = i-1;
+                }
+            }
+        }
+    }
+}
